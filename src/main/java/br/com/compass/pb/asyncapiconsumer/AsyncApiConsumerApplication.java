e6e5f4f74282dba7f265008a907ce34edc5c2b6b
Thiago Odilon de Almeida <thiago-almeida05@hotmail.com>
2023-08-07 18:23:39 -0300

    :tada: First Commit

---
+++ src/main/java/br/com/compass/pb/asyncapiconsumer/AsyncApiConsumerApplication.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.asyncapiconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncApiConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncApiConsumerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/compass/pb/asyncapiconsumer/AsyncApiConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.compass.pb.asyncapiconsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncApiConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
